2c06033d6a76b3dd7ac4d7ca1192faf4a6ce82f0
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-05-09 00:34:14 +0200

    master: Breaking changes, add AppCore library as dependency.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- coregraphics-base/CGRenderer.c
+++
@@ -1,7 +0,0 @@
-/*
- * Created on Wed May 08 2024
- *
- * Copyright (c) 2024 SoftwareLabs B.V
- */
-
-#include <CGRenderer.h>
--- coregraphics-base/CGRenderer.h
+++
@@ -1,9 +0,0 @@
-/*
- * Created on Wed May 08 2024
- *
- * Copyright (c) 2024 SoftwareLabs B.V
- */
-
-#pragma once
-
-#include <coregraphics-base/CGTypes.h>
