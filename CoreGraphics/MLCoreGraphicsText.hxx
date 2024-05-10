47781171427c4a41dde46fa5afdeb7745601157e
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-05-10 08:39:44 +0200

    master: CG: MHR-23: initial commit.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

---
+++ CoreGraphics/MLCoreGraphicsText.hxx
@@ -0,0 +1,13 @@
+/*
+ * Created on Thu May 09 2024
+ *
+ * Copyright (c) 2024 SoftwareLabs B.V
+ */
+
+#pragma once
+
+#include <Foundation.hxx>
+#include <MLFont.hxx>
+
+class MLCoreGraphicsText;
+
