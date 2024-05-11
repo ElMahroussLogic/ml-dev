229e5e033f87a8dfd06ecda6a9630391ae680162
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-05-11 08:40:51 +0200

    MHR-23: Add gaussian blur (taken from cookbook).
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

---
+++ CoreAnimation/MLKeyFrame.hxx
@@ -0,0 +1,7 @@
+/*
+ * Created on Sat May 11 2024
+ *
+ * Copyright (c) 2024 SystemLabs B.V
+ */
+
+#pragma once
--- CoreGraphics/MLCoreGraphicsContext.cxx
+++ CoreGraphics/MLCoreGraphicsContext.cxx
@@ -5,27 +5,35 @@
  */
 
 #include <MLCoreGraphicsContext.hxx>
-#include <MLCoreGraphicsContextCairo.inl>
+#include <Private/MLCoreGraphicsContextCairo.inl>
 
 /// @brief Request a context regarding the features set.
 /// @param featureSet the feature set.
 /// @param strict should we require all the features to be here?
-/// @note if no, please check for feature with operator& inside MLCoreGraphicsContext.
+/// @note if no, please check for feature with operator& inside
+/// MLCoreGraphicsContext.
 /// @return The new graphics context.
-ML_IMPORT MLCoreGraphicsContext* CGRequestContext(CGSizeT featureSet, bool strict,
-                                                  CGReal width, CGReal height)
-{
-    MLCoreGraphicsContext* context = new MLCoreGraphicsContextCairo(width, height);
-    return context;
+ML_IMPORT MLCoreGraphicsContext *
+CGRequestContext(CGSizeT featureSet, bool strict, CGReal width, CGReal height) {
+  MLCoreGraphicsContext *context =
+      new MLCoreGraphicsContextCairo(width, height);
+  return context;
 }
 
 /// @brief Releases a Core Graphics context.
 /// @param context The context itself.
 /// @return This function returns void.
-ML_IMPORT void CGReleaseContext(MLCoreGraphicsContext* context)
-{
-    if (context)
-    {
-        delete context;
-    }
+ML_IMPORT void CGReleaseContext(MLCoreGraphicsContext *context) {
+  if (context) {
+    delete context;
+  }
+}
+
+/// @brief To string method.
+/// @return the class as a string
+const MLString MLCoreGraphicsContext::toString() {
+  MLString str = MLString(strlen("MLCoreGraphicsContext"));
+  str += "MLCoreGraphicsContext";
+
+  return str;
 }
