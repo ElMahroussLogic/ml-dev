4bb9fc12da9ea7b00fba7d54a92b585f409c7125
amlal <amlalelmahrouss@icloud.com>
2024-06-07 18:26:27 +0200

    MHR-23: Add lerp formula.
    Signed-off-by: amlal <amlalelmahrouss@icloud.com>

--- CoreAnimation/MLCoreAnimation.hxx
+++ CoreAnimation/MLCoreAnimation.hxx
@@ -7,3 +7,16 @@
 #pragma once
 
 #include <Foundation.hxx>
+#include <CoreGraphics.hxx>
+
+/// @brief Linear interop helper function.
+/// @param start where to begin
+/// @param end where to end
+/// @param status the status
+/// @return the returned value.
+inline CGReal CGLerp(float start, float end, float status)
+{
+    return (start + status * (end - start));
+}
+
+// END OF HEADER.
