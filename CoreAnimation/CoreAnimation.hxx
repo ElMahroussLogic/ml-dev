7567f62cac8fdbc23e80b9f49f39bba076d1947d
Amlal EL Mahrouss <amlalelmahrouss@icloud.com>
2024-06-18 12:58:42 +0200

    imp: final commit on branch.
    Signed-off-by: Amlal EL Mahrouss <amlalelmahrouss@icloud.com>

--- CoreAnimation/CoreAnimation.hxx
+++ CoreAnimation/CoreAnimation.hxx
@@ -8,7 +8,7 @@
 
 #ifdef ML_WRAP_AROUND_NS
 /// @note this is for the purpose of having a common namespace alongside zeta's codebase.
-namespace ma::zeta::api::animation
+namespace fr::zeta::api::animation
 {
 #endif
 
--- CoreGraphics/CoreGraphics.hxx
+++ CoreGraphics/CoreGraphics.hxx
@@ -2,7 +2,7 @@
 
 #ifdef ML_WRAP_AROUND_NS
 /// @note this is for the purpose of having a common namespace alongside zeta's codebase.
-namespace ma::zeta::api::graphics
+namespace fr::zeta::api::graphics
 {
 #endif
 
