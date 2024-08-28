81c803bb6fb8d89fcfdac7ae7e9f28b4ae7c3d86
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-08-28 16:36:06 +0200

    [IMP] Moved driver signing tool to ZKA, replaced this with a executable signing tool.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- tools/ExecSignMain.cxx
+++ tools/ExecSignMain.cxx
@@ -5,24 +5,27 @@
  */
 
 #include <cstdint>
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
 #include <filesystem>
 
-#define kDriverSignedExt   ".sigg"
-#define kDriverExt		   ".sys"
-#define kSignedDriverMagic "SIGG"
+#define kDriverSignedExt   ".exes"
+#define kDriverExt		   ".exe"
+#define kSignedDriverMagic "SEFF"
 
 namespace details
 {
-	struct SIGNED_DRIVER_HEADER final
+	struct SIGNED_EXEC_HEADER final
 	{
+	    // end of executable zone.
+	    char d_binary_padding_end_of_exec[512];
 		// doesn't change.
 		char d_binary_magic[5];
 		int	 d_binary_version;
-		// can change.
+		// can change according to version.
 		char		  d_binary_name[4096];
 		std::uint64_t d_binary_checksum;
 		std::uint64_t d_binary_size;
@@ -46,8 +49,9 @@ int main(int argc, char* argv[])
 	{
 		if (strcmp(argv[i], "/?") == 0)
 		{
-			std::cout << "drvsign: ZKA Driver Signing Tool.\n";
-			std::cout << "drvsign: © ZKA Technologies, all rights reserved.\n";
+			std::cout << "execsign: ZKA Executable Signing Tool.\n";
+			std::cout << "execsign: Use this tool to indicate that this executable is made for ZKA.\n";
+			std::cout << "execsign: © ZKA Technologies, all rights reserved.\n";
 
 			return 0;
 		}
@@ -57,7 +61,7 @@ int main(int argc, char* argv[])
 			!std::string(argv[1]).ends_with(kDriverExt))
 			return -1;
 
-	details::SIGNED_DRIVER_HEADER sig{0};
+	details::SIGNED_EXEC_HEADER sig{0};
 
 	sig.d_binary_version = 1;
 
@@ -66,7 +70,8 @@ int main(int argc, char* argv[])
 
 	sig.d_binary_size = std::filesystem::file_size(argv[1]);
 
-	memset(sig.d_binary_padding, 0xFF, 512);
+	memset(sig.d_binary_padding, 0x00, 512);
+	memset(sig.d_binary_padding_end_of_exec, 0x00, 512);
 
 	sig.d_binary_checksum = 0;
 
@@ -83,22 +88,23 @@ int main(int argc, char* argv[])
 	if (!details::drvsign_check_for_mz(ss.str()))
 	{
 		std::filesystem::remove(signed_path);
-		std::cout << "drvsign: Couldn't sign current driver, Input driver isn't a valid executable.\n";
+		std::cout << "execsign: Couldn't sign current driver, Input executable isn't a valid.\n";
 
 		return 1;
 	}
 
 	for (auto ch : ss.str())
 	{
-		sig.d_binary_checksum |= ch;
+		sig.d_binary_checksum |= (ch * rand());
 	}
 
-	sig.d_binary_checksum ^= sig.d_binary_size;
+	sig.d_binary_checksum |= sig.d_binary_size;
 
-	of_drv.write((char*)&sig, sizeof(details::SIGNED_DRIVER_HEADER));
 	of_drv.write(ss.str().c_str(), ss.str().size());
+	of_drv.write((char*)&sig, sizeof(details::SIGNED_EXEC_HEADER));
 
-	std::cout << "drvsign: Signing is done, quiting, here is the key: " << sig.d_binary_checksum << ".\n";
+
+	std::cout << "execsign: Signing is done, quiting, here is the key: " << sig.d_binary_checksum << ".\n";
 
 	return 0;
 }
