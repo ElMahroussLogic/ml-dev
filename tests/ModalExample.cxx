4dde7ee834a881d0c55b21869d042c539684086c
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-10-12 14:12:09 +0200

    IMP: Add ZKA version of the sample (Which isn't using the GTK)
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- tests/ModalExample.cxx
+++ tests/ModalExample.cxx
@@ -6,19 +6,23 @@
 
 #include <FoundationKit/MLAlert.hxx>
 
+#ifndef __NDK__
 extern "C"
 {
 #include <gtk/gtk.h>
 #include <gdk/gdk.h>
 #include <gdk/gdkkeysyms.h>
 }
+#endif
 
 int main(int argc, char** argv)
 {
-    gtk_init(&argc, &argv);
+#ifndef __NDK__
+	gtk_init(&argc, &argv);
+#endif
 
-    MLAlert alert;
-    alert.runModal("ZT Framework", "%s", "Modal on ZKA.");
+	MLAlert alert;
+	alert.runModal("ZT Framework", "%s", "Modal on ZKA.");
 
-    return 0;
+	return 0;
 }
