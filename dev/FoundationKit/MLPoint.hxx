2c90051d11cffc862318e3a7e169d6c52f96dd40
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-10-21 10:21:47 +0200

    IMP: Add isWithin method to MLPoint.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- dev/FoundationKit/MLPoint.hxx
+++ dev/FoundationKit/MLPoint.hxx
@@ -8,10 +8,20 @@
 
 #include <FoundationKit/Foundation.hxx>
 
+/// @brief A X, Y point within a display.
 struct MLPoint
 {
 	MLReal x;
 	MLReal y;
+
+	/// @brief Check if point is within the current MLPoint.
+	/// @param point the current point to check.
+	/// @retval true if point is within this point.
+	/// @retval validations failed.
+	bool isWithin(MLPoint& point)
+	{
+	    return point.x > x && point.y > y;
+	}
 
 	operator bool()
 	{
