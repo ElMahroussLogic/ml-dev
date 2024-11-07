b52a6d9fa95de2b89eee9b5570cb4d2319ea5686
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-11-07 20:02:08 +0100

    META: BUMP.

--- dev/AnimationKit/Foundation.cc
+++ dev/AnimationKit/Foundation.cc
@@ -1,7 +1,7 @@
 /*
  * Created on Fri May 17 2024
  *
- * Copyright (c) 2024 EL Mahrouss Logic
+ * Copyright (c) 2024 Amlal EL Mahrouss
  */
 
 #include <AnimationKit/Foundation.h>
--- dev/AnimationKit/Foundation.h
+++ dev/AnimationKit/Foundation.h
@@ -1,6 +1,6 @@
 /*
  * AnimationKit
- * Copyright (c) 2024 EL Mahrouss Logic.
+ * Copyright (c) 2024 Amlal EL Mahrouss.
  */
 
 #pragma once
